bf121151e48face6330bacc14e152188f8b4c3f6
Hang Yoko Tran <hihihehe0321@gmail.com>
2019-11-21 18:27:49 +0700

    edit config.py and .env

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ load_dotenv()
 
 class Config(object):
     DEBUG=True
-    SQLALCHEMY_DATABASE_URL=os.environ.get('DATABASE_URL')
+    SQLALCHEMY_DATABASE_URI=os.environ.get('DATABASE_URL')
     SECRET_KET='freakingtiredkey'
 
 
